def85d0b0fb410d6a42a2182502ec7efc4567faa
drewp <drewp@bigasterisk.com>
2010-07-25 11:33:31 -0700

    upgrade to sparqlhttp. doesnt all work yet
    Ignore-this: fc7d8627185aa24cf8ceaa04abef2101
    
    darcs-hash:20100725183331-312f9-24f866321aaf249dbd81897efeb17b49ce3d137c.gz

--- mpg.py
+++ mpg.py
@@ -13,18 +13,36 @@ from rdflib import RDF, RDFS
 
 from gasuse import DC, DATE, GAS, DOLLAR, MILE, GALLON, TYPE
 
+# http://wiki.python.org/moin/PythonDecoratorLibrary#EasyDumpofFunctionArguments
+def dump_args(func):
+    "This decorator dumps out the arguments passed to a function before calling it"
+    argnames = func.func_code.co_varnames[:func.func_code.co_argcount]
+    fname = func.func_name
+    def echo_func(*args,**kwargs):
+        print fname, ":", ', '.join(
+            '%s=%r' % entry
+            for entry in zip(argnames,args) + kwargs.items())
+        ret = func(*args, **kwargs)
+        print " ->", ret
+        return ret
+    return echo_func
+
 def fillUps(graph, car):
     """sorted list of (date, fillUp)
 
     should sort by odometer if date is missing
     """
-    ret = list(graph.query("""SELECT ?date ?odo ?fillUp WHERE { 
-                      ?fillUp a gas:FillUp; gas:odometer ?odo; dc:date ?date; gas:car ?car .
-   }""", initNs=dict(gas=GAS, dc=DC), initBindings={Variable("?car") : car}))
-    ret.sort(key=lambda r: (str(r[0]), str(r[1]))) # alpha sort on odo
+    ret = list(graph.queryd("""
+      SELECT ?date ?odo ?fillUp WHERE { 
+        ?fillUp a gas:FillUp ;
+          gas:odometer ?odo ;
+          dc:date ?date ;
+          gas:car ?car .
+      }""", initBindings={"car" : car}))
+    ret.sort(key=lambda r: (str(r['date']), str(r['odo']))) # alpha sort on odo
 
     print "return %s fillups for %s" % (len(ret), car)
-    return [(f[0], f[2]) for f in ret]
+    return [(f['date'], f['fillUp']) for f in ret]
 
 def shorten(graph, uri):
     for prefix, namespace in graph.namespaces():
@@ -36,46 +54,81 @@ def dump(graph, subject):
     for p,o in sorted(graph.predicate_objects(f)):
         print "  %s: %s" % (shorten(graph, p), shorten(graph, o))
 
+def milesOnFillup(graph, f, prevFillup):
+    """
+    number of miles driven between the last fillup and this one
+    """
+    val = graph.value
+    try:
+        # prefer odometer since it should have less per-reading error
+        miles = (int(val(f, GAS['odometer'])) -
+                 int(val(prevFillup, GAS['odometer'])))
+        if miles < 1 or miles > 600:
+            raise ValueError(miles)
+        return miles
+    except (ValueError, TypeError), e:
+        return val(f, GAS['tripMeter'])
+
+def floatVal(graph, subj, pred):
+    v = graph.value(subj, pred)
+    if v is None:
+        raise TypeError(v)
+    v = v.strip().rstrip('?')
+    try:
+        return float(v)
+    except TypeError:
+        print "%s %s %s" % (subj, pred, v)
+        raise
+
+def fillupStatements(graph, fillup, prevFillup):
+    """
+    statements about this fillup
+    """
+    stmts = set()
+    val = graph.value
+    miles = milesOnFillup(graph, fillup, prevFillup)
+
+    if miles is not None:
+        stmts.add((prevFillup, GAS['milesOnThisTank'],
+                   Literal(miles, datatype=MILE)))
+    else:
+        miles = floatVal(graph, prevFillup, GAS['milesOnThisTank'])
+
+    try:
+        mpg = (miles / floatVal(graph, prevFillup, GAS['gallons']))
+        stmts.add((prevFillup, GAS['mpgOnThisTank'],
+                   Literal(mpg, datatype=TYPE['mpg'])))
+    except (ValueError, TypeError), e:
+        print vars()
+        raise
+
+    try:
+        dpm = (float(val(val(prevFillup, GAS["gasPrice"]),
+                         GAS["pricePerGallon"])) / mpg)
+        stmts.add((prevFillup, GAS['dollarsPerMile'],
+                   Literal(dpm, datatype=DOLLAR)))
+    except (ValueError, TypeError), e:
+        print vars()
+        raise
+    return stmts
+
 def calcMpg(graph, car):
     prevFillup = None
     val = graph.value
+        
+    stmts = set()
     for d,f in fillUps(graph, car):
         if prevFillup is not None:
-            miles = None
             try:
-                # prefer odometer since it should have less per-reading error
-                miles = (int(val(f, GAS['odometer'])) -
-                         int(val(prevFillup, GAS['odometer'])))
-                if miles < 1 or miles > 600:
-                    raise ValueError
-            except (ValueError, TypeError), e:
-                try:
-                    miles = val(f, GAS['tripMeter'])
-                except (ValueError, TypeError), e:
-                    pass
-            if miles is not None:
-                graph.add((prevFillup, GAS['milesOnThisTank'],
-                           Literal(miles, datatype=MILE)))
-
-            try:
-                mpg = (float(val(prevFillup,
-                                 GAS['milesOnThisTank'])) /
-                       float(val(prevFillup, GAS['gallons'])))
-                graph.add((prevFillup, GAS['mpgOnThisTank'],
-                           Literal(mpg, datatype=TYPE['mpg'])))
-            except (ValueError, TypeError), e:
-                pass
-
-            try:
-                dpm = (float(val(val(prevFillup, GAS["gasPrice"]),
-                                 GAS["pricePerGallon"])) /
-                       float(val(prevFillup, GAS["mpgOnThisTank"])))
-                graph.add((prevFillup, GAS['dollarsPerMile'],
-                           Literal(dpm, datatype=DOLLAR)))
-            except (ValueError, TypeError), e:
-                pass
+                stmts.update(fillupStatements(graph, f, prevFillup))
+            except TypeError, e:
+                print "skip prevFillup=%s, %s" % (prevFillup, e)
         prevFillup = f
-    
+        # these get queried in the next loop
+        graph.add(stmts, context=GAS['computed'])
+
+def clearComputed(graph):
+    graph.subgraphClear(GAS['computed'])
 
 if __name__ == '__main__':
     from rdflib.sparql.sparqlGraph import SPARQLGraph as Graph
